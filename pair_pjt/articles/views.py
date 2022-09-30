5072f3fb0d2e45271bd6a077c9459d2680cd3420
jiyeong99 <1999wldud@gmail.com>
2022-09-30 11:03:17 +0900

    review

--- pair_pjt/articles/views.py
+++ pair_pjt/articles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from pair_pjt.settings import BASE_DIR
 
 # Create your views here.
+def index(request):
+    reviews = Review.objects.all()
+    context = {
+        "reviews" : reviews,
+    }
+    return render(request, 'articles/index.html',context)
--- pair_pjt/pair_pjt/settings.py
+++ pair_pjt/pair_pjt/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pair_pjt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'tempates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
