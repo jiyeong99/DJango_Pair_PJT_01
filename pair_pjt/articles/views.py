a41e2261c6d1b9975a8316e5f0c0b9556e706ade
rhoeunbin <rhoeb21@gmail.com>
2022-09-30 13:47:03 +0900

    화이팅

--- pair_pjt/articles/views.py
+++ pair_pjt/articles/views.py
@@ -9,3 +9,26 @@ def index(request):
         "reviews" : reviews,
     }
     return render(request, 'articles/index.html',context)
+
+def create(request):
+
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    Review.objects.create(title=title, content=content)
+
+    context = {
+        "title" : title,
+        "content" : content,
+    }
+    return redirect('articles:index')
+
+
+def new(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    context = {
+        "title" : title,
+        "content" : content,
+    }
+    return render(request,'articles/new.html', context)
+
